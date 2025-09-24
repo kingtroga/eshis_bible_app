514f8f9677defc27c320957404094f94f189a262
kingtroga <trogaclassicman@gmail.com>
2025-09-24 02:58:57 +0100

    change stuff

--- wordsofchrist/urls.py
+++ wordsofchrist/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/08087422585/', admin.site.urls),
     path('', include('quotes.urls')),
 ]
 
